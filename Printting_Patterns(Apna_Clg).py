984ed29235fb1821f5c58218a7063299541273b5
Abdullah Haroon <vendettafearless7@gmail.com>
2026-02-27 17:58:36 +0500

    star patterns

---
+++ Printting_Patterns(Apna_Clg).py
@@ -0,0 +1,15 @@
+x = "*****"
+for i in range(4):
+    print(x)
+                                               #code for solid rectangle of stars
+print("----------------------------------------------------------------------------------------")
+
+for i in range(4):
+  for j in range(5):
+    if(i==0 or i==(3) or j==0 or j==(4)):
+        print("*", end="")
+    else:
+        print(" ", end="")
+  print()
+          #code for hallow rectangle
+print("----------------------------------------------------------------------------------------")
