b858441dfb5b7c5339abf76f1fca0d0a4c023ded
Abdullah Haroon <vendettafearless7@gmail.com>
2026-02-27 22:38:12 +0500

    hallow pattern base on  user input

--- Printting_Patterns(Apna_Clg).py
+++ Printting_Patterns(Apna_Clg).py
@@ -13,3 +13,17 @@ for i in range(4):
   print()
           #code for hallow rectangle
 print("----------------------------------------------------------------------------------------")
+
+
+row = int(input("Enter number of rows="))
+col = int(input("Enter number of columns="))
+for i in range(row ):
+    for j in range(col):
+        if(i==0 or i==(row -1) or j==0 or j==(col -1)):
+         print("*", end="")
+        else:
+         print(" ", end="")
+    print()
+
+
+print("----------------------------------------------------------------------------------------")
